f3d9f596131492ac11de9dd34a9ab914a55ea10e
Thspli <thalissondouglas86@gmail.com>
2025-06-03 17:02:01 -0300

    script

--- public/pages/areas/script.js
+++ public/pages/areas/script.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const area = "Mistieri";
-    const token = localStorage.getItem('token');
+    const urlParams = new URLSearchParams(window.location.search);
+    const area = urlParams.get('area');
+    const token = String(localStorage.getItem('token').replaceAll('"', ''));
+    console.log(token);
     const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
         method: 'GET',
         headers: {
-            "Content-type": "application/json"
+            "Content-type": "application/json",
+            Authorization: "Bearer " + token
         },
-        authorization: "Bearer " + token
     });
     const data = await response.json();
-});
+    console.log(data);
+
+    console.log("data:", data);
+    console.log("data.resultados:", data.Resultado);
+
+    for (var brinquedo of data.Resultado) {
+        console.log(brinquedo);
+
+        const grid = document.querySelector(".rides-grid");
+        grid.innerHTML = grid.innerHTML +
+         `
+            <div class="ride-card">
+                <div class="ride-image" style="background-image: url('imagem/bugabalum.jpg')"></div>
+                <div class="ride-info">
+                    <h3 class="ride-name">${brinquedo.name}</h3>
+                    <div class="ride-time">${brinquedo.waiting_time}</div>
+                    <span class="ride-status status-busy">${brinquedo.status}</span>
+                </div>
+            </div>`;
+    }
+});x
